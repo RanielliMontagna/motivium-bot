1747fda3b3386ef08d6e6e880797e006aaa055b2
Ranielli Montagna <raniellimontagna@hotmail.com>
2025-04-18 09:40:04 -0300

    feat: :sparkles: add weather API integration and update environment configuration

---
+++ src/services/weather/weatherService.ts
@@ -0,0 +1,4 @@
+export const weatherApiUrl = 'http://api.weatherapi.com/v1'
+
+export const currentWeather = weatherApiUrl + '/current.json'
+export const forecastWeather = weatherApiUrl + '/forecast.json'
